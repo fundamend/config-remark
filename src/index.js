d092edd3ea56d82db4d7681091d151a655eafb15
- <gitkraken@mail.decouplr.com>
2022-01-05 06:46:11 +0100

    feat: add gfm plugin to support github formatted markdown

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import frontmatter from 'remark-frontmatter';
+import gfm from 'remark-gfm';
 import lint from '@fundamend/config-remark-lint';
 import retext from '@fundamend/config-remark-retext';
 
-const plugins = [[frontmatter, 'yaml'], lint, retext];
+const plugins = [[frontmatter, 'yaml'], gfm, lint, retext];
 const settings = {};
 
 export default {
