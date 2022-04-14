4d0e5971a5e4d4386f496a03f0cc4266af1e07aa
- <gitkraken@mail.decouplr.com>
2022-04-14 13:49:13 +0200

    fix: remove some differences between prettier, remark-stringify, and remark plugins

--- src/index.js
+++ src/index.js
@@ -4,7 +4,14 @@ import lint from '@fundamend/config-remark-lint';
 import retext from '@fundamend/config-remark-retext';
 
 const plugins = [[frontmatter, 'yaml'], gfm, lint, retext];
-const settings = {};
+const settings = {
+	// Settings for https://github.com/remarkjs/remark/tree/main/packages/remark-stringify
+	// Some of these interfere with remark lint plugins
+	bullet: '-',
+	emphasis: '_',
+	listItemIndent: 'one',
+	tightDefinitions: true
+};
 
 export default {
 	plugins: plugins,
